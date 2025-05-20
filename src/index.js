6c6a4299dc0081c0aa8e86316c2a3ba750eb78b1
Casper Gamborg <Gamborgc@gmail.com>
2025-05-20 23:42:08 +0200

    computer change

--- src/index.js
+++ src/index.js
@@ -7,9 +7,38 @@ const pic3 = document.getElementById("picture3");
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("previous");
 
+const dots = document.getElementsByClassName("dot");
+
 let counter = 0;
 
 let pictures = [pic1, pic2, pic3];
+
+for (let n = 0; n < dots.length; n++) {
+  dots[n].addEventListener("click", () => {
+    pictures.forEach((pic) => {
+      pic.style.display = "none";
+    });
+
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].style.backgroundColor = "gray";
+    }
+    dots[n].style.backgroundColor = "blue";
+    pictures[n].style.display = "block";
+  });
+}
+
+function picChange() {
+  pictures[counter].style.display = "none";
+
+  counter++;
+  if (counter >= pictures.length) {
+    counter = 0;
+  }
+  pictures[counter].style.display = "block";
+  dots[counter].style.backgroundColor = "blue";
+}
+
+setInterval(picChange, 1000);
 
 nextBtn.addEventListener("click", () => {
   pictures[counter].style.display = "none";
@@ -17,21 +46,14 @@ nextBtn.addEventListener("click", () => {
   if (counter >= pictures.length) {
     counter = 0;
   }
-  console.log(pictures[counter]);
-  console.log("Count: ", counter);
   pictures[counter].style.display = "block";
 });
 
-console.log(counter);
 prevBtn.addEventListener("click", () => {
   pictures[counter].style.display = "none";
   counter--;
-
   if (counter < 0) {
     counter = pictures.length - 1;
   }
-  console.log(pictures[counter]);
-  console.log("Count: ", counter);
-
   pictures[counter].style.display = "block";
 });
